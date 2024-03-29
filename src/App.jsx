3eb3ad8246d54fca6ffb2a56944021968a0a035f
Sergio Jurado <sajm9116@gmail.com>
2024-03-28 19:05:49 -0500

    finalizado

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css';
+import arrayPhrases from './utils/phrases.json';
+import getRandom from './services/getRandom';
+import { useState } from 'react';
+import ShowPhrase from './components/ShowPhrase';
+import ButtonPhrase from './components/ButtonPhrase';
+import arrayImages from './utils/images.json';
+
+function App() {
+
+  const [phrase, setPhrase] = useState(getRandom(arrayPhrases));
+
+ const [image, setImage] = useState(getRandom(arrayImages));
+
+  const objStyles = {
+    backgroundImage: `url(../public/assets/Fondos2/fondo${image}.png)`,
+  }
+
+  return (
+    <div className='app' style={objStyles}>
+      <h1 className='app__title'>Galletas De La Fortuna</h1>
+      <ButtonPhrase
+        setPhrase={setPhrase}
+        setImage={setImage}
+      />
+      <ShowPhrase
+        phrase={phrase}
+      />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/ShowPhrase.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ShowPhrase = ({phrase}) => {
+
+  return (
+    <div className='text'>
+        <p>{phrase.phrase}</p>
+        <p>Fuente: {phrase.author}</p>
+    </div>
+  )
+}
+
+export default ShowPhrase
